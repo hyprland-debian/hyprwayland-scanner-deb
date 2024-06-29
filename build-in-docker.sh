2d50674c9885b1a6d616d9dfb0ea80bd59838ead
Ilya Bylich <ibylich@gmail.com>
2024-06-29 16:20:29 +0200

    define version once for the entire workflow

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,25 +2,28 @@
 
 set -eux
 
-export VER="0.3.10"
+if [[ ! -v BUILD_VERSION ]]; then
+    echo "BUILD_VERSION is not set"
+    exit 1
+fi
 
 mkdir /build
 cd /build
 
-wget https://github.com/hyprwm/hyprwayland-scanner/archive/refs/tags/v$VER.tar.gz -O hyprwayland-scanner-$VER.tar.gz
-tar -xzmf hyprwayland-scanner-$VER.tar.gz
-cd hyprwayland-scanner-$VER
+wget https://github.com/hyprwm/hyprwayland-scanner/archive/refs/tags/v$BUILD_VERSION.tar.gz -O hyprwayland-scanner-$BUILD_VERSION.tar.gz
+tar -xzmf hyprwayland-scanner-$BUILD_VERSION.tar.gz
+cd hyprwayland-scanner-$BUILD_VERSION
 
-cp -r /shared/debian /build/hyprwayland-scanner-$VER/debian
-sed -i "s/VERSION_TEMPLATE/$VER/g" /build/hyprwayland-scanner-$VER/debian/changelog
-sed -i "s/VERSION_TEMPLATE/$VER/g" /build/hyprwayland-scanner-$VER/debian/control
+cp -r /shared/debian /build/hyprwayland-scanner-$BUILD_VERSION/debian
+sed -i "s/VERSION_TEMPLATE/$BUILD_VERSION/g" /build/hyprwayland-scanner-$BUILD_VERSION/debian/changelog
+sed -i "s/VERSION_TEMPLATE/$BUILD_VERSION/g" /build/hyprwayland-scanner-$BUILD_VERSION/debian/control
 dpkg-buildpackage -us -uc
 
 cd /build
 ls -l
 
-cp hyprwayland-scanner_$VER\_amd64.deb /shared
+cp hyprwayland-scanner_$BUILD_VERSION\_amd64.deb /shared
 
 cd /shared
-dpkg-deb -c hyprwayland-scanner_$VER\_amd64.deb
-dpkg -I hyprwayland-scanner_$VER\_amd64.deb
+dpkg-deb -c hyprwayland-scanner_$BUILD_VERSION\_amd64.deb
+dpkg -I hyprwayland-scanner_$BUILD_VERSION\_amd64.deb
